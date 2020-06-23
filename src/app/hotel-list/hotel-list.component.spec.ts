dd716804c967c7b1275b23adc98c1b3e25109e34
Billy Lando <billy_lando@yahoo.com>
2020-06-23 11:03:49 +0200

    :art: feat: add template explainations

---
+++ src/app/hotel-list/hotel-list.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { HotelListComponent } from './hotel-list.component';
+
+describe('HotelListComponent', () => {
+  let component: HotelListComponent;
+  let fixture: ComponentFixture<HotelListComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ HotelListComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(HotelListComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/hotel-list/hotel-list.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hotel-list',
+  templateUrl: './hotel-list.component.html',
+  styleUrls: ['./hotel-list.component.css']
+})
+export class HotelListComponent implements OnInit {
+
+  public hotels: any[] = [
+    {
+      hotelId: 1,
+      hotelName: 'Buea sweet life',
+      description: 'Belle vue au bord de la mer',
+      price: 230.5,
+      imageUrl: 'assets/img/hotel-room.jpg'
+    }, {
+      hotelId: 2,
+      hotelName: 'Marakech',
+      description: 'Profitez de la vue sur les montagnes',
+      price: 145.5,
+      imageUrl: 'assets/img/the-interior.jpg'
+    }, {
+      hotelId: 3,
+      hotelName: 'Buea sweet life',
+      description: 'Belle vue au bord de la mer',
+      price: 120.12,
+      imageUrl: 'assets/img/indoors.jpg'
+    }, {
+      hotelId: 4,
+      hotelName: 'Cape town city',
+      description: 'Magnifique cadre pour votre séjour',
+      price: 135.12,
+      imageUrl: 'assets/img/window.jpg'
+    }
+  ]
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
