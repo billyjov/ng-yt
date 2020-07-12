2a5e52914160e6bd1dce197f2a7f56391787423c
Billy Lando <billy_lando@yahoo.com>
2020-07-12 18:50:13 +0200

    feat(pipes): add built in pipes & register locale fr

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { registerLocaleData } from '@angular/common';
+import localeFr from '@angular/common/locales/fr';
 
 import { AppComponent } from './app.component';
 import { HotelListComponent } from './hotel-list/hotel-list.component';
+
+
+registerLocaleData(localeFr, 'fr');
 
 @NgModule({
   declarations: [
