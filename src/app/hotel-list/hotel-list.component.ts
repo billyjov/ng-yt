3a3d3139448abaaf3fc02d45bbef4df41578c982
Billy Lando <billy_lando@yahoo.com>
2020-07-17 12:58:57 +0200

    feat: add model for hotels

--- src/app/hotel-list/hotel-list.component.ts
+++ src/app/hotel-list/hotel-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { IHotel } from './hotel';
 
 @Component({
   selector: 'app-hotel-list',
@@ -7,7 +8,7 @@ import { Component } from '@angular/core';
 })
 export class HotelListComponent {
 
-  public hotels: any[] = [
+  public hotels: IHotel[] = [
     {
       hotelId: 1,
       hotelName: 'Buea sweet life',
---
+++ src/app/hotel-list/hotel.ts
@@ -0,0 +1,7 @@
+export interface IHotel {
+  hotelId: number;
+  hotelName: string;
+  description: string;
+  price: number;
+  imageUrl: string;
+}
